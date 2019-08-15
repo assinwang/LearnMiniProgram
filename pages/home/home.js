fdd8949f0f9323b808fef4b220349fd1c5bf30cd
wanggangqiang <johnsonwang@skylinematrix.com>
2019-08-15 14:46:29 +0800

    注册App和Page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,63 @@
 // pages/home/home.js
+const app =  getApp()
+console.log(app.globalData.name)
+console.log(app.globalData.age)
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  //2.初始化数据
+  data:{
+    list:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 1.监听页面的生命周期函数
+  //页面被加载出来
+  onLoad(){
+    console.log('onLoad')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success: (res)=>{
+        console.log(res)
+        const data = res.data
+        this.setData({
+          list: data
+        })
+      }
+    })
+  },
+  //页面显示出来时
+  onShow() {
+    console.log('onShow')
+  },
+  //页面初次渲染完成时
+  onReady() {
+    console.log('onReady')
+  },
+  //当页面隐藏起来时
+  onHide(){
+    console.log('onHide')
+  },
+  onUnload(){
+    console.log('onUnload')
+  },
+  //3.监听wxml中相关的事件
+  handleGetUserInfo(event) {
+    console.log(event)
+  },
+  handleViewClick(){
+    console.log('页面被点击了')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //4.监听其他事件
+  //监听页面的滚动
+  onPageScroll(obj){
+    console.log(obj)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //监听页面滚动到底部
+  onReachBottom(){
+    console.log('页面滚动到底部')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //监听下拉刷新
+  onPullDownRefresh(){
+    console.log('下拉刷新事件')
   }
 })
