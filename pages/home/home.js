7e58fd256552aaeb2c20a3238627115566333ed8
wanggangqiang <johnsonwang@skylinematrix.com>
2019-08-20 19:01:34 +0800

    弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,74 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中...',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: function(){
+        console.log('弹窗成功')
+      },
+      fail: function(){
+        console.log('弹窗失败')
+      },
+      complete: function(){
+        console.log('完成函数调用')
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function(res){
+        console.log(res)
+        if(res.cancel){
+          console.log('用户取消')
+        }
+        if(res.confirm){
+          console.log('用户确定')
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载...',
+      mask: true
+    })
 
+    setTimeout(()=>{
+      //必须手动hideLoading才会让loading消失
+      wx.hideLoading()
+    },1000)
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleActionSheet(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor: 'green',
+      success:function(res){
+        console.log(res)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return{
+      title: '你好',
+      path: '/pages/about/about',
+      imageUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566307484887&di=647fae3a9deca7ef787e8593a242ab14&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201305%2F26%2F20130526140022_5fMJe.jpeg'
+    }
   }
 })
